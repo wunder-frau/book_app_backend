ed3ece966339d65326115377fbdcd69a03365a1d
irene <wunder.frau@yandex.by>
2025-02-15 23:31:44 +0200

    Centralize the application error handling in middleware and enable middleware express-async-errors

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,69 +1,84 @@
 const router = require("express").Router();
 const { Blog } = require("../models");
 
-// Middleware to find a blog by ID
 const blogFinder = async (req, res, next) => {
-  req.blog = await Blog.findByPk(req.params.id);
-  if (!req.blog) return res.status(404).json({ error: "Blog not found" });
-  next();
+  try {
+    req.blog = await Blog.findByPk(req.params.id);
+    if (!req.blog) {
+      return next(new Error("Blog not found"));
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
 
-router.get("/", async (req, res) => {
+// GET all blogs
+router.get("/", async (req, res, next) => {
   try {
     const blogs = await Blog.findAll();
     res.json(blogs);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch blogs" });
+    next(error);
   }
 });
 
+// GET a specific blog by ID
 router.get("/:id", blogFinder, async (req, res) => {
-  if (req.blog) {
-    res.json(req.blog);
-  } else {
-    res.status(404).json({ error: "Blog not found" });
-  }
-});
-
-router.post("/", async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body);
-    return res.status(201).json(blog);
-  } catch (error) {
-    return res.status(400).json({ error: "Failed to create blog" });
-  }
-});
-
-router.delete("/:id", blogFinder, async (req, res) => {
-  if (req.blog) {
-    await req.blog.destroy();
-    return res.status(204).end();
-  } else {
-    return res.status(404).json({ error: "Blog not found" });
-  }
-});
-
-router.put("/:id", blogFinder, async (req, res) => {
-  const { likes } = req.body;
-
-  if (likes !== 1 && likes !== -1) {
-    return res
-      .status(400)
-      .json({ error: "You can only increment or decrement by 1" });
-  }
-
-  if (typeof likes !== "number") {
-    return res.status(400).json({ error: "Likes must be a number" });
-  }
-
-  const likesCount = req.blog.likes + likes;
-  if (likesCount < 0) {
-    return res.status(400).json({ error: "Likes cannot be negative" });
-  }
-  req.blog.likes = likesCount;
-  await req.blog.save({ fields: ["likes"] });
-  console.log("After Update:", req.blog.likes);
   res.json(req.blog);
+});
+
+// POST a new blog (Centralized error handling)
+router.post("/", async (req, res, next) => {
+  try {
+    const { author, url, title, likes } = req.body;
+
+    if (!author || !url || !title) {
+      return next(new Error("Author, URL, and Title are required"));
+    }
+
+    const blog = await Blog.create({ author, url, title, likes });
+    res.status(201).json(blog);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE a blog by ID
+router.delete("/:id", blogFinder, async (req, res, next) => {
+  try {
+    await req.blog.destroy();
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// UPDATE blog likes
+router.put("/:id", blogFinder, async (req, res, next) => {
+  try {
+    const { likes } = req.body;
+
+    if (typeof likes !== "number") {
+      return next(new Error("Likes must be a number"));
+    }
+
+    if (likes !== 1 && likes !== -1) {
+      return next(new Error("You can only increment or decrement by 1"));
+    }
+
+    const newLikes = req.blog.likes + likes;
+    if (newLikes < 0) {
+      return next(new Error("Likes cannot be negative"));
+    }
+
+    req.blog.likes = newLikes;
+    await req.blog.save({ fields: ["likes"] });
+
+    res.json(req.blog);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("express-async-errors");
 const app = express();
 
 const { PORT } = require("./util/config");
@@ -6,9 +7,10 @@ const { connectToDatabase } = require("./util/db");
 
 const blogsRouter = require("./controllers/blogs");
 
+const errorHandler = require("./middleware/errorHandler");
 app.use(express.json());
-
 app.use("/api/blogs", blogsRouter);
+app.use(errorHandler);
 
 const start = async () => {
   await connectToDatabase();
@@ -18,80 +20,3 @@ const start = async () => {
 };
 
 start();
-// require("dotenv").config();
-
-// const { Sequelize, Model, DataTypes } = require("sequelize");
-// const express = require("express");
-// const app = express();
-// app.use(express.json());
-
-// const sequelize = new Sequelize(process.env.DATABASE_URL);
-
-// class Blog extends Model {}
-
-// Blog.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     author: {
-//       type: DataTypes.TEXT,
-//       allowNull: false,
-//     },
-//     url: {
-//       type: DataTypes.TEXT,
-//       allowNull: false,
-//     },
-//     title: {
-//       type: DataTypes.TEXT,
-//       allowNull: false,
-//     },
-//     likes: {
-//       type: DataTypes.INTEGER,
-//       defaultValue: 0,
-//     },
-//   },
-//   {
-//     sequelize,
-//     underscored: true,
-//     timestamps: false,
-//     modelName: "blogs",
-//   }
-// );
-
-// app.get("/api/blogs", async (req, res) => {
-//   const notes = await Blog.findAll();
-//   res.json(notes);
-// });
-
-// app.post("/api/blogs", async (req, res) => {
-//   try {
-//     const note = await Blog.create(req.body);
-//     return res.json(note);
-//   } catch (error) {
-//     return res.status(400).json({ error });
-//   }
-// });
-
-// app.delete("/api/blogs/:id", async (req, res) => {
-//   try {
-//     const blog = await Blog.findByPk(req.params.id);
-
-//     if (!blog) {
-//       return res.status(404).json({ error: "Blog not found" });
-//     }
-
-//     await blog.destroy(req.params.id);
-//     return res.json(note);
-//     res.status(204).end();
-//   } catch (error) {
-//     res.status(500).json({ error: "Failed to delete blog" });
-//   }
-// });
-
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
